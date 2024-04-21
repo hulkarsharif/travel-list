47b7b405f4d3f049afb56acc8b88d52f9b41f1f9
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-04-21 17:49:09 -0400

    Add initialItems

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false }
+];
 const App = () => {
     return (
         <div className="app">
