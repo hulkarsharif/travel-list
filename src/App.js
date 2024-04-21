2767f71b7d6408485986b1e13d19e59b58501634
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-04-20 22:37:40 -0400

    Create components

---
+++ src/App.js
@@ -0,0 +1,35 @@
+const App = () => {
+    return (
+        <div className="app">
+            <Logo />
+            <Form />
+            <PackingList />
+            <Stats />
+        </div>
+    );
+};
+
+export default App;
+
+const Logo = () => {
+    return <h1>🌴Far Away 💼</h1>;
+};
+const Form = () => {
+    return (
+        <div className="add-form">
+            <h3>What do you need for your 😍 trip?</h3>
+        </div>
+    );
+};
+const PackingList = () => {
+    return <div className="list">List</div>;
+};
+const Stats = () => {
+    return (
+        <footer className="stats">
+            <em>
+                💼You have X items on your list, and you already packed X (X%)
+            </em>
+        </footer>
+    );
+};
