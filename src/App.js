e9f7b8ed4bccfac96e2cfc0c99756bd73151f961
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-04-23 19:34:14 -0400

    Add ne item state

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ const initialItems = [
     { id: 3, description: "Charger", quantity: 1, packed: false }
 ];
 const App = () => {
+    const [items, setItems] = useState([]);
+
+    function handleAddItems(item) {
+        setItems((items) => [...items, item]);
+    }
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingList />
+            <Form onAddItems={handleAddItems} />
+            <PackingList items={items} />
             <Stats />
         </div>
     );
@@ -21,7 +27,7 @@ export default App;
 const Logo = () => {
     return <h1>🌴Far Away 💼</h1>;
 };
-const Form = () => {
+const Form = ({ onAddItems }) => {
     const [description, setDescription] = useState("");
     const [quantity, setQuantity] = useState(1);
 
@@ -37,6 +43,10 @@ const Form = () => {
             id: Date.now()
         };
         console.log(newItem);
+        onAddItems(newItem);
+
+        setDescription("");
+        setQuantity(1);
     };
 
     return (
@@ -62,11 +72,11 @@ const Form = () => {
         </form>
     );
 };
-const PackingList = () => {
+const PackingList = ({ items }) => {
     return (
         <div className="list">
             <ul>
-                {initialItems.map((item) => (
+                {items.map((item) => (
                     <Item item={item} key={item.id} />
                 ))}
             </ul>
